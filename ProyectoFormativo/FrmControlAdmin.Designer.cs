0ed0f88ecd96d6e5951bf45d79e9c36e2b23c15e
Jhorman <jhorman.arias.9@gmail.com>
2022-02-28 23:57:59 -0500

    Revert commit terminado otra vez sombra777

--- ProyectoFormativo/FrmControlAdmin.Designer.cs
+++ ProyectoFormativo/FrmControlAdmin.Designer.cs
@@ -143,6 +143,7 @@ namespace ProyectoFormativo
             this.cb_Fecha_RAdmin = new System.Windows.Forms.CheckBox();
             this.combox_pagAdmin = new System.Windows.Forms.ComboBox();
             this.DGVReportesAdmin = new System.Windows.Forms.DataGridView();
+<<<<<<< HEAD
             this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -151,6 +152,8 @@ namespace ProyectoFormativo
             this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+=======
+>>>>>>> parent of ce1b872 (terminado otra vez)
             this.groupBox8 = new System.Windows.Forms.GroupBox();
             this.label9 = new System.Windows.Forms.Label();
             this.lbl_horaRAdminReporte = new System.Windows.Forms.Label();
@@ -163,6 +166,17 @@ namespace ProyectoFormativo
             this.lbl_Rol = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.lbl_Nom_User = new System.Windows.Forms.Label();
+<<<<<<< HEAD
+=======
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+>>>>>>> parent of ce1b872 (terminado otra vez)
             this.tc_Usuario.SuspendLayout();
             this.Usuarios.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -304,7 +318,11 @@ namespace ProyectoFormativo
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.Location = new System.Drawing.Point(647, 211);
             this.label8.Name = "label8";
+<<<<<<< HEAD
             this.label8.Size = new System.Drawing.Size(26, 16);
+=======
+            this.label8.Size = new System.Drawing.Size(25, 16);
+>>>>>>> parent of ce1b872 (terminado otra vez)
             this.label8.TabIndex = 21;
             this.label8.Text = "De";
             // 
@@ -322,7 +340,11 @@ namespace ProyectoFormativo
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.Location = new System.Drawing.Point(533, 211);
             this.label10.Name = "label10";
+<<<<<<< HEAD
             this.label10.Size = new System.Drawing.Size(51, 16);
+=======
+            this.label10.Size = new System.Drawing.Size(50, 16);
+>>>>>>> parent of ce1b872 (terminado otra vez)
             this.label10.TabIndex = 19;
             this.label10.Text = "Pagina";
             // 
@@ -466,6 +488,7 @@ namespace ProyectoFormativo
             // 
             // txtBuscar_VerU
             // 
+<<<<<<< HEAD
             this.txtBuscar_VerU.Location = new System.Drawing.Point(16, 34);
             this.txtBuscar_VerU.Name = "txtBuscar_VerU";
             this.txtBuscar_VerU.Size = new System.Drawing.Size(159, 20);
@@ -473,6 +496,12 @@ namespace ProyectoFormativo
             this.txtBuscar_VerU.Text = "Documento:";
             this.txtBuscar_VerU.Enter += new System.EventHandler(this.txtBuscar_VerU_Enter);
             this.txtBuscar_VerU.Leave += new System.EventHandler(this.txtBuscar_VerU_Leave);
+=======
+            this.txtBuscar_VerU.Location = new System.Drawing.Point(16, 30);
+            this.txtBuscar_VerU.Name = "txtBuscar_VerU";
+            this.txtBuscar_VerU.Size = new System.Drawing.Size(159, 20);
+            this.txtBuscar_VerU.TabIndex = 10;
+>>>>>>> parent of ce1b872 (terminado otra vez)
             // 
             // btnBuscar_verU
             // 
@@ -481,11 +510,19 @@ namespace ProyectoFormativo
             this.btnBuscar_verU.FlatAppearance.BorderColor = System.Drawing.SystemColors.MenuHighlight;
             this.btnBuscar_verU.FlatAppearance.BorderSize = 0;
             this.btnBuscar_verU.ForeColor = System.Drawing.SystemColors.HighlightText;
+<<<<<<< HEAD
             this.btnBuscar_verU.Location = new System.Drawing.Point(182, 30);
             this.btnBuscar_verU.Name = "btnBuscar_verU";
             this.btnBuscar_verU.Size = new System.Drawing.Size(99, 30);
             this.btnBuscar_verU.TabIndex = 0;
             this.btnBuscar_verU.Text = "Buscar Usuario";
+=======
+            this.btnBuscar_verU.Location = new System.Drawing.Point(205, 30);
+            this.btnBuscar_verU.Name = "btnBuscar_verU";
+            this.btnBuscar_verU.Size = new System.Drawing.Size(99, 30);
+            this.btnBuscar_verU.TabIndex = 0;
+            this.btnBuscar_verU.Text = "Buscar";
+>>>>>>> parent of ce1b872 (terminado otra vez)
             this.btnBuscar_verU.UseVisualStyleBackColor = false;
             this.btnBuscar_verU.Click += new System.EventHandler(this.btnBuscar_verU_Click);
             // 
@@ -810,7 +847,11 @@ namespace ProyectoFormativo
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(647, 211);
             this.label6.Name = "label6";
+<<<<<<< HEAD
             this.label6.Size = new System.Drawing.Size(26, 16);
+=======
+            this.label6.Size = new System.Drawing.Size(25, 16);
+>>>>>>> parent of ce1b872 (terminado otra vez)
             this.label6.TabIndex = 17;
             this.label6.Text = "De";
             // 
@@ -829,7 +870,11 @@ namespace ProyectoFormativo
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(533, 211);
             this.label7.Name = "label7";
+<<<<<<< HEAD
             this.label7.Size = new System.Drawing.Size(51, 16);
+=======
+            this.label7.Size = new System.Drawing.Size(50, 16);
+>>>>>>> parent of ce1b872 (terminado otra vez)
             this.label7.TabIndex = 15;
             this.label7.Text = "Pagina";
             // 
@@ -1190,7 +1235,11 @@ namespace ProyectoFormativo
             this.lbl_horaSistemAdmin.Font = new System.Drawing.Font("MS Reference Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_horaSistemAdmin.Location = new System.Drawing.Point(6, 16);
             this.lbl_horaSistemAdmin.Name = "lbl_horaSistemAdmin";
+<<<<<<< HEAD
             this.lbl_horaSistemAdmin.Size = new System.Drawing.Size(330, 44);
+=======
+            this.lbl_horaSistemAdmin.Size = new System.Drawing.Size(329, 44);
+>>>>>>> parent of ce1b872 (terminado otra vez)
             this.lbl_horaSistemAdmin.TabIndex = 0;
             this.lbl_horaSistemAdmin.Text = "Hora del sistema:";
             // 
@@ -1365,7 +1414,11 @@ namespace ProyectoFormativo
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(666, 273);
             this.label5.Name = "label5";
+<<<<<<< HEAD
             this.label5.Size = new System.Drawing.Size(26, 16);
+=======
+            this.label5.Size = new System.Drawing.Size(25, 16);
+>>>>>>> parent of ce1b872 (terminado otra vez)
             this.label5.TabIndex = 13;
             this.label5.Text = "De";
             // 
@@ -1441,6 +1494,7 @@ namespace ProyectoFormativo
             this.DGVReportesAdmin.TabIndex = 7;
             this.DGVReportesAdmin.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.DGVReportesAdmin_CellFormatting);
             // 
+<<<<<<< HEAD
             // dataGridViewTextBoxColumn2
             // 
             this.dataGridViewTextBoxColumn2.DataPropertyName = "id_control";
@@ -1498,6 +1552,8 @@ namespace ProyectoFormativo
             this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
             this.dataGridViewTextBoxColumn8.ReadOnly = true;
             // 
+=======
+>>>>>>> parent of ce1b872 (terminado otra vez)
             // groupBox8
             // 
             this.groupBox8.Controls.Add(this.label9);
@@ -1514,7 +1570,11 @@ namespace ProyectoFormativo
             this.label9.Font = new System.Drawing.Font("MS Reference Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.Location = new System.Drawing.Point(5, 16);
             this.label9.Name = "label9";
+<<<<<<< HEAD
             this.label9.Size = new System.Drawing.Size(330, 44);
+=======
+            this.label9.Size = new System.Drawing.Size(329, 44);
+>>>>>>> parent of ce1b872 (terminado otra vez)
             this.label9.TabIndex = 0;
             this.label9.Text = "Hora del sistema:";
             // 
@@ -1535,7 +1595,11 @@ namespace ProyectoFormativo
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(552, 273);
             this.label1.Name = "label1";
+<<<<<<< HEAD
             this.label1.Size = new System.Drawing.Size(51, 16);
+=======
+            this.label1.Size = new System.Drawing.Size(50, 16);
+>>>>>>> parent of ce1b872 (terminado otra vez)
             this.label1.TabIndex = 11;
             this.label1.Text = "Pagina";
             // 
@@ -1620,6 +1684,66 @@ namespace ProyectoFormativo
             this.lbl_Nom_User.Text = "nombre";
             this.lbl_Nom_User.Visible = false;
             // 
+<<<<<<< HEAD
+=======
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "id_control";
+            this.dataGridViewTextBoxColumn2.FillWeight = 70F;
+            this.dataGridViewTextBoxColumn2.HeaderText = "ID";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Nombre";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Visitante";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            // 
+            // Column9
+            // 
+            this.Column9.DataPropertyName = "Documento";
+            this.Column9.HeaderText = "Documento";
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "Tipobien";
+            this.dataGridViewTextBoxColumn4.HeaderText = "Bien";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            // 
+            // Column8
+            // 
+            this.Column8.DataPropertyName = "Serial";
+            this.Column8.HeaderText = "Serial";
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.DataPropertyName = "hora_entrada";
+            this.dataGridViewTextBoxColumn6.HeaderText = "Entrada";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn7
+            // 
+            this.dataGridViewTextBoxColumn7.DataPropertyName = "hora_salida";
+            this.dataGridViewTextBoxColumn7.HeaderText = "Salida";
+            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            this.dataGridViewTextBoxColumn7.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn8
+            // 
+            this.dataGridViewTextBoxColumn8.DataPropertyName = "Nom_User";
+            this.dataGridViewTextBoxColumn8.HeaderText = "Usuario";
+            this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
+            this.dataGridViewTextBoxColumn8.ReadOnly = true;
+            // 
+>>>>>>> parent of ce1b872 (terminado otra vez)
             // FrmControlAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
